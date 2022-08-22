cfa25fc6fe3d68bc6b02a56494a9db73fec00186
khanh pham <phamngocbaokhanh2015@gmail.com>
2022-08-22 23:29:46 +0700

    feat: demo react router dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 
 {
   /* <ul>
@@ -41,7 +42,10 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <Router>
+    <App />
+  </Router>
+
   // </React.StrictMode>
 );
 
